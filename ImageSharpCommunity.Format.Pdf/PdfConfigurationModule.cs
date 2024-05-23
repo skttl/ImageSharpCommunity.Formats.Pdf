1dd43e8890c3d9f830850c26c771b8f5a6607fdd
Søren Kottal <sk@ecreo.dk>
2024-05-23 12:31:31 +0200

    adds unimplemented encoder

--- ImageSharpCommunity.Format.Pdf/PdfConfigurationModule.cs
+++ ImageSharpCommunity.Format.Pdf/PdfConfigurationModule.cs
@@ -8,6 +8,7 @@ namespace ImageSharpCommunity.Format.Pdf
         public void Configure(Configuration configuration)
         {
             configuration.ImageFormatsManager.SetDecoder(PdfFormat.Instance, PdfDecoder.Instance);
+            configuration.ImageFormatsManager.SetEncoder(PdfFormat.Instance, new PdfEncoder());
             configuration.ImageFormatsManager.AddImageFormatDetector(new PdfFormatDetector());
         }
     }
---
+++ ImageSharpCommunity.Format.Pdf/PdfEncoder.cs
@@ -0,0 +1,13 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+
+namespace ImageSharpCommunity.Format.Pdf
+{
+    public class PdfEncoder : ImageEncoder
+    {
+        protected override void Encode<TPixel>(Image<TPixel> image, Stream stream, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
